8f019de0bc898815e51d5fc6eb535a7456434cab
salted26 <151119318+salted26@users.noreply.github.com>
2024-08-14 12:45:42 +0900

    일부 화면 수정

--- src/component/Box.js
+++ src/component/Box.js
@@ -5,7 +5,11 @@ const Box = (props) => {
   const imageUrl = () => {
     if(props.item !== undefined) {
       return (
-        <img src={props.item && props.item.url} alt={props.item?.name}/>
+        <img src={props.item && props.item.url} alt={props.item?.name} className="content"/>
+      )
+    } else {
+      return (
+        <div className="content"> 가위 바위 보를 선택해주세요! </div>
       )
     }
   }
